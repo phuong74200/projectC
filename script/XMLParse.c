d09f75e7f5866e6b050c4931588e55cd078d6fb1
unknown <phuong74200@gmail.com>
2021-02-04 16:08:44 +0700

    update

--- script/XMLParse.c
+++ script/XMLParse.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 char *getXMLValue(char *tag, char *XMLBuffer, char *tagValue) {
@@ -49,6 +50,20 @@ char *getXMLValue(char *tag, char *XMLBuffer, char *tagValue) {
 
 int main() {
     char res[100];
-    getXMLValue("h1", "<h1>Hello</h1><fuong>ProjectCUsername</fuong>", &res);
+    getXMLValue("h1", "<h1>Hello</h1><h1>fuong</h1><fuong>ProjectCUsername</fuong>", &res);
     printf("%s", res);
+
+    FILE *f = fopen("script\\client.c", "rb");
+    fseek(f, 0, SEEK_END);
+    long fsize = ftell(f);
+    fseek(f, 0, SEEK_SET); /* same as rewind(f); */
+
+    char *string = malloc(fsize + 1);
+    fread(string, 1, fsize, f);
+    printf("%s", string);
+    fclose(f);
+
+    string[fsize] = 0;
+
+    printf("%s", string);
 }
