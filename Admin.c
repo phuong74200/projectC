f473896a3a6b89598115bea007d6cbe6456bf75f
ChariskYuri <78487345+ChariskYuri@users.noreply.github.com>
2021-02-08 10:19:38 +0700

    Name Code applied

--- Admin.c
+++ Admin.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct item
+struct product
 {
 	char name[1000];
 	int id;
@@ -13,50 +13,50 @@ struct item
 	unsigned int currentQuantity;
 	unsigned int soldQuantity;
 	unsigned int profit;
-} item[1000];
+} product[1000];
 
 int index = 0;
 
-void writeXMLFile(struct item item[], int index)
+void writeXMLFile(struct product product[], int index)
 {
 	FILE *xmlFilePrt;
 	char xmlInput[1000] = "<name>";
-	char addressFile[1000] = "database\\products\\";
-	char idstring[10];
-	itoa(item[index].id, idstring, 10);
-	strcat(strcat(xmlInput, idstring), "</name>");
-	xmlFilePrt = fopen(strcat(strcat(addressFile, idstring), ".xml"), "w");
+	char filePath[1000] = "database\\products\\";
+	char idString[10];
+	itoa(product[index].id, idString, 10);
+	strcat(strcat(xmlInput, idString), "</name>");
+	xmlFilePrt = fopen(strcat(strcat(filePath, idString), ".xml"), "w");
 	fprintf(xmlFilePrt, "%s", xmlInput);
 	fclose(xmlFilePrt);
 }
 
-void inputName(struct item item[], int index)
+void inputName(struct product product[], int index)
 {
-	char idstring[10];
-	char addressFile[1000] = "E:\\Mine\\Study\\C\\Projects\\Tet\\ItemFiles\\";
+	char idString[10];
+	char filePath[1000] = "E:\\Mine\\Study\\C\\Projects\\Tet\\ItemFiles\\";
 	printf("Nhap ten mat hang: ");
 	fflush(stdin);
-	fgets(item[index].name, sizeof(item[index].name), stdin);
-	itoa(item[index].id, idstring, 10);
-	FILE *itemFilePtr;
-	itemFilePtr = fopen(strcat(strcat(addressFile, idstring), ".txt"), "a");
-	fprintf(itemFilePtr, "Ten san pham: %s", item[index].name);
-	fclose(itemFilePtr);
+	fgets(product[index].name, sizeof(product[index].name), stdin);
+	itoa(product[index].id, idString, 10);
+	FILE *productFilePtr;
+	itemFilePtr = fopen(strcat(strcat(filePath, idString), ".txt"), "a");
+	fprintf(productFilePtr, "Ten san pham: %s", product[index].name);
+	fclose(productFilePtr);
 }
 
-void inputID(struct item item[], int index)
+void inputID(struct product[], int index)
 {
 	char validation;
 	int checker;
-	char idstring[10];
-	char addressFile[1000] = "E:\\Mine\\Study\\C\\Projects\\Tet\\ItemFiles\\";
+	char idString[10];
+	char filePath[1000] = "E:\\Mine\\Study\\C\\Projects\\Tet\\ItemFiles\\";
 	do
 	{
 		do
 		{
 			printf("Nhap so ID cua mat hang: ");
 			fflush(stdin);
-			scanf("%u", &item[index].id);
+			scanf("%u", &product[index].id);
 			scanf("%c", &validation);
 			if (validation != 10)
 				printf("Nhap loi. Xin nhap lai.\n");
@@ -64,100 +64,100 @@ void inputID(struct item item[], int index)
 		while (validation != 10);
 		for (checker = 0; checker < index; checker++)
 		{
-			if (item[checker].id == item[index].id)
+			if (product[checker].id == product[index].id)
 			{
-				printf("Da co mat hang co ID %d \n", item[index].id);
+				printf("Da co mat hang co ID %d \n", product[index].id);
 				break;
 			}
 		}
 	}
 	while (checker != index);
-	itoa(item[index].id, idstring, 10);
-	FILE *itemFilePtr;
-	itemFilePtr = fopen(strcat(strcat(addressFile, idstring), ".txt"), "w");
-	fprintf(itemFilePtr, "ID: %d \n", item[index].id);
-	fclose(itemFilePtr);
+	itoa(product[index].id, idString, 10);
+	FILE *productFilePtr;
+	productFilePtr = fopen(strcat(strcat(filePath, idString), ".txt"), "w");
+	fprintf(productFilePtr, "ID: %d \n", product[index].id);
+	fclose(productFilePtr);
 }
 
-void inputPrice(struct item item[], int index)
+void inputPrice(struct product product[], int index)
 {
 	char validation;
-	char idstring[10];
-	char addressFile[1000] = "E:\\Mine\\Study\\C\\Projects\\Tet\\ItemFiles\\";
+	char idString[10];
+	char filePath[1000] = "E:\\Mine\\Study\\C\\Projects\\Tet\\ItemFiles\\";
 	do
 	{
 		printf("Nhap don gia: ");
 		fflush(stdin);
-		scanf("%u", &item[index].price);
+		scanf("%u", &product[index].price);
 		scanf("%c", &validation);
 		if (validation != 10)
 			printf("Nhap loi. Xin nhap lai.\n");
 	}
 	while (validation != 10);
-	itoa(item[index].id, idstring, 10);
-	FILE *itemFilePtr;
-	itemFilePtr = fopen(strcat(strcat(addressFile, idstring), ".txt"), "a");
-	fprintf(itemFilePtr, "Don gia: %d\n", item[index].price);
-	fclose(itemFilePtr);
+	itoa(product[index].id, idString, 10);
+	FILE *productFilePtr;
+	productFilePtr = fopen(strcat(strcat(filePath, idString), ".txt"), "a");
+	fprintf(productFilePtr, "Don gia: %d\n", product[index].price);
+	fclose(productFilePtr);
 }
 
-void inputDiscount(struct item item[], int index)
+void inputDiscount(struct product product[], int index)
 {
 	char validation;
-	char idstring[10];
-	char addressFile[1000] = "E:\\Mine\\Study\\C\\Projects\\Tet\\ItemFiles\\";
+	char idString[10];
+	char filePath[1000] = "E:\\Mine\\Study\\C\\Projects\\Tet\\ItemFiles\\";
 	do
 	{
 		printf("Nhap muc giam gia (theo phan tram): ");
 		fflush(stdin);
-		scanf("%u", &item[index].discount);
+		scanf("%u", &product[index].discount);
 		scanf("%c", &validation);
-		if (validation != 10 || item[index}.discount < 0 || item[index}.discount >= 100)
+		if (validation != 10 || product[index].discount < 0 || product[index].discount >= 100)
 			printf("Nhap loi. Xin nhap lai.\n");
 	}
-	while (validation != 10 || item[index}.discount < 0 || || item[index}.discount >= 100);
-	item[index].price = item[index].price - item[index].price * item[index].discount / 100;
-	itoa(item[index].id, idstring, 10);
-	FILE *itemFilePtr;
-	itemFilePtr = fopen(strcat(strcat(addressFile, idstring), ".txt"), "a");
-	fprintf(itemFilePtr, "Muc giam gia: %d%%\n", item[index].discount);
-	fclose(itemFilePtr);
+	while (validation != 10 || product[index].discount < 0 || product[index].discount >= 100);
+	product[index].price = product[index].price - product[index].price * product[index].discount / 100;
+	itoa(product[index].id, idString, 10);
+	FILE *productFilePtr;
+	productFilePtr = fopen(strcat(strcat(filePath, idString), ".txt"), "a");
+	fprintf(productFilePtr, "Muc giam gia: %d%%\n", product[index].discount);
+	fclose(productFilePtr);
 }
 
-void inputCurrentQuantity(struct item item[], int index)
+void inputCurrentQuantity(struct product product[], int index)
 {
 	char validation;
-	char idstring[10];
-	char addressFile[1000] = "E:\\Mine\\Study\\C\\Projects\\Tet\\ItemFiles\\";
+	char idString[10];
+	char filePath[1000] = "E:\\Mine\\Study\\C\\Projects\\Tet\\ItemFiles\\";
 	do
 	{
 		printf("Nhap so luong: ");
 		fflush(stdin);
-		scanf("%u", &item[index].currentQuantity);
+		scanf("%u", &product[index].currentQuantity);
 		scanf("%c", &validation);
 		if (validation != 10)
 			printf("Nhap loi. Xin nhap lai.\n");
 	}
 	while (validation != 10);
-	itoa(item[index].id, idstring, 10);
-	FILE *itemFilePtr;
-	itemFilePtr = fopen(strcat(strcat(addressFile, idstring), ".txt"), "a");
-	fprintf(itemFilePtr, "So luong: %d\n", item[index].currentQuantity);
-	fclose(itemFilePtr);
+	itoa(product[index].id, idString, 10);
+	FILE *productFilePtr;
+	productFilePtr = fopen(strcat(strcat(filePath, idString), ".txt"), "a");
+	fprintf(productFilePtr, "So luong: %d\n", product[index].currentQuantity);
+	fclose(productFilePtr);
 }
 
-void inputNewItem(struct item item[], int *indexPtr)
+void inputNewProduct(struct product product[], int *indexPtr)
 {
 	char confirmation;
 	do
 	{
-		inputID(item, *indexPtr);
-		inputName(item, *indexPtr);
-		inputPrice(item, *indexPtr);
-		inputDiscount(item, *indexPtr);
-		inputCurrentQuantity(item, *indexPtr);
+		inputID(product, *indexPtr);
+		inputName(product, *indexPtr);
+		inputPrice(product, *indexPtr);
+		inputDiscount(product, *indexPtr);
+		inputCurrentQuantity(product, *indexPtr);
 		(*indexPtr)++;
-		printf("Ban co muon tiep tuc nhap hang khong? (Y/N)");
+		printf("Ban co muon tiep tuc nhap hang khong? (Y/N): ");
 		fflush(stdin);
 		scanf("%c", &confirmation);
 		while (confirmation != 'Y' && confirmation != 'y' && confirmation != 'N' && confirmation != 'n')
@@ -170,9 +170,9 @@ void inputNewItem(struct item item[], int *indexPtr)
 	while (confirmation == 'Y' || confirmation == 'y');
 }
 
-void deleteItem(struct item item[], int *indexPtr)
+void deleteProduct(struct product product[], int *indexPtr)
 {
-	int id, checker, arraydeleter;
+	int id, checker, arrayDeleter;
 	char validation, confirmation;
 	if (index == 0)
 	{
@@ -195,21 +195,21 @@ void deleteItem(struct item item[], int *indexPtr)
 			}
 			for (checker = 0; checker <= index; checker++)
 			{
-				if (item[checker].id == id)
+				if (product[checker].id == id)
 				{
-					char idstring[10];
-					char addressFile[1000] = "E:\\Mine\\Study\\C\\Projects\\Tet\\ItemFiles\\";
-					itoa(item[checker].id, idstring, 10);
-					remove(strcat(strcat(addressFile, idstring), ".txt"));
-					for (arraydeleter = checker; arraydeleter < index; arraydeleter++)
+					char idString[10];
+					char filePath[1000] = "E:\\Mine\\Study\\C\\Projects\\Tet\\ItemFiles\\";
+					itoa(product[checker].id, idString, 10);
+					remove(strcat(strcat(filePath, idString), ".txt"));
+					for (arrayDeleter = checker; arrayDeleter < index; arrayDeleter++)
 					{
-						item[arraydeleter].id = item[arraydeleter + 1].id;
-						strcpy(item[arraydeleter + 1].name, item[arraydeleter].name);
-						item[arraydeleter].price = item[arraydeleter + 1].price;
-						item[arraydeleter].discount = item[arraydeleter + 1].discount;
-						item[arraydeleter].currentQuantity = item[arraydeleter + 1].currentQuantity;
-						item[arraydeleter].soldQuantity = item[arraydeleter + 1].soldQuantity;
-						item[arraydeleter].profit = item[arraydeleter + 1].profit;
+						product[arrayDeleter].id = product[arrayDeleter + 1].id;
+						strcpy(product[arrayDeleter + 1].name, product[arrayDeleter].name);
+						product[arrayDeleter].price = product[arrayDeleter + 1].price;
+						product[arrayDeleter].discount = product[arrayDeleter + 1].discount;
+						product[arrayDeleter].currentQuantity = product[arrayDeleter + 1].currentQuantity;
+						product[arrayDeleter].soldQuantity = product[arrayDeleter + 1].soldQuantity;
+						product[arrayDeleter].profit = product[arrayDeleter + 1].profit;
 					}
 					break;
 				}
@@ -218,7 +218,7 @@ void deleteItem(struct item item[], int *indexPtr)
 				(*indexPtr)--;
 			else
 				printf("Khong co mat hang nao co ID %d \n", id);
-			printf("Ban co muon tiep tuc xoa hang khong? (Y/N)");
+			printf("Ban co muon tiep tuc xoa hang khong? (Y/N): ");
 			fflush(stdin);
 			scanf("%c", &confirmation);
 			while (confirmation != 'Y' && confirmation != 'y' && confirmation != 'N' && confirmation != 'n')
@@ -252,12 +252,12 @@ int main()
 		{
 			case 1:
 			{
-				inputNewItem(item, &index);
+				inputNewProduct(product, &index);
 				break;
 			}
 			case 2:
 			{
-				deleteItem(item, &index);
+				deleteProduct(product, &index);
 				break;
 			}
 			case 8:
