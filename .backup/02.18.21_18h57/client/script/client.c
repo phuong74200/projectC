0827d518c783df120278d2a612039eb4b3b38222
unknown <phuong74200@gmail.com>
2021-02-18 19:02:35 +0700

    seperate server folder/ client folder

---
+++ .backup/02.18.21_18h57/client/script/client.c
@@ -0,0 +1,25 @@
+ #include <gtk/gtk.h>
+
+ int main (int argc, char *argv[])
+ {
+  GtkWidget *window;
+  
+   /* Initialise GTK+ passing to it all command line arguments  */
+   gtk_init (&argc, &argv);
+  
+   /* create a new window, set values */
+   window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+   gtk_window_set_title (GTK_WINDOW (window), "Hello GTK+ World");
+   /* set the size of the window */
+   gtk_widget_set_size_request (GTK_WIDGET (window), 400, 400);
+   /* connect the windows "destroy" event */
+   g_signal_connect (G_OBJECT (window), "destroy",
+      G_CALLBACK (gtk_main_quit), NULL);  
+  
+   /* set the window as visible */
+   gtk_widget_show (window);
+  
+   /* run the GTK+ main loop */
+   gtk_main ();
+   return 0;
+ }
